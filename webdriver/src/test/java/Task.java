29731e1b82f6fc0605233476d255535ebc83471e
Dmitry Ashurko <dima_ashurko@gmail.com>
2019-05-22 01:05:31 +0300

    second push for an automation task

--- webdriver/src/test/java/Task.java
+++ webdriver/src/test/java/Task.java
@@ -7,17 +7,32 @@ public class Task {
 
     public static void main(String[] args) throws InterruptedException {
 
+        //Original version of the code
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://cloud.google.com/");
         //risky point
-        WebElement clickProducts = driver.findElement(By.xpath("//*[@data-label='Footer GCP pricing'][@track-name='productsAndPricing']"));
+        WebElement clickProducts = driver.findElement(By.xpath("//*[@data-label='Footer See all products']"));
         clickProducts.click();
         //risky point
-        WebElement clickPricing = driver.findElement(By.xpath("//*[@track-name='seePricing']"));
+        WebElement clickPricing = driver.findElement(By.xpath("//*[@data-label='Footer GCP pricing']")); //заглушка. решение неверно!
         clickPricing.click();
+        //risky point
+        WebElement openList = driver.findElement(By.xpath("//*[@class='cloud-jump-menu__toggle']"));
+        openList.click();
+        //risky point
+        WebElement clickCalculator = driver.findElement(By.xpath("//a[text()='Calculators']"));
+        clickCalculator.click();
+        //risky point
+        //WebElement clickComputeEngine = driver.findElement(By.linkText("Compute Engine")); // попробую обойтись без этого. Мб сделать проверку, что выбран этот тип
+        //clickComputeEngine.click();
         //risky point
         Thread.sleep(2000);
         driver.quit();
+
+
+        //Завтра попробовать полностью заполнить списки в Калькуляторе, устранить проблему с See Pricing, Compute Engine
+
 
     }
 }
